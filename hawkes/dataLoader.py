fdf52c85e341ee7f5c1a6da64d69e654f5718aa6
konqr <konark145@gmail.com>
2023-10-06 19:11:52 +0100

    loader

---
+++ hawkes/dataLoader.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import os
+import itertools as it
+import time
+import datetime as dt
+import numpy as np
+
+
+class Loader():
+    # Message file information:
+    # ----------------------------------------------------------
+    #
+    #   - Dimension:    (NumberEvents x 6)
+    #
+    #   - Structure:    Each row:
+    #                   Time stamp (sec after midnight with decimal
+    #                   precision of at least milliseconds and
+    #                   up to nanoseconds depending on the period),
+    #                   Event type, Order ID, Size (# of shares),
+    #                   Price, Direction
+    #
+    #                   Event types:
+    #                       - '1'   Submission new limit order
+    #                       - '2'   Cancellation (partial)
+    #                       - '3'   Deletion (total order)
+    #                       - '4'   Execution of a visible limit order
+    #                       - '5'   Execution of a hidden limit order
+    # 			- '7'   Trading Halt (Detailed
+    #                               information below)
+    #
+    #                   Direction:
+    #                       - '-1'  Sell limit order
+    #                       - '-2'  Buy limit order
+    #                       - NOTE: Execution of a sell (buy)
+    #                               limit order corresponds to
+    #                               a buyer-(seller-) initiated
+    #                               trade, i.e. a BUY (SELL) trade.
+    #
+    # ----------------------------------------------------------
+
+    # Orderbook file information:
+    # ----------------------------------------------------------
+    #
+    #   - Dimension:    (NumberEvents x (NumberLevels*4))
+    #
+    #   - Structure:    Each row:
+    #                   Ask price 1, Ask volume 1, Bid price 1,
+    #                   Bid volume 1, Ask price 2, Ask volume 2,
+    #                   Bid price 2, Bid volume 2, ...
+    #
+    #   - Note:         Unoccupied bid (ask) price levels are
+    #                   set to -9999999999 (9999999999) with volume 0.
+    #
+    # ----------------------------------------------------------
+
+    def __init__(self, ric, sDate, eDate, **kwargs):
+        self.ric = ric
+        self.sDate = sDate
+        self.eDate = eDate
+        self.nlevels = kwargs.get("nlevels", 10)
+        self.dataPath = "D:\\Work\\PhD\\Data\\"
+
+    def load(self):
+        data = []
+        ric = self.ric.split(".")[0]
+        for d in pd.date_range(self.sDate, self.eDate):
+            theMessageBookFileName = self.dataPath + ric + "_" + d.strftime("%Y-%m-%d") + "_34200000_57600000_message_10.csv"
+            theOrderBookFileName = self.dataPath +  ric + "_" + d.strftime("%Y-%m-%d") + "_34200000_57600000_orderbook_10.csv"
+            theMessageBook = pd.read_csv(theMessageBookFileName,
+                                         names=['Time', 'Type', 'OrderID', 'Size', 'Price', 'TradeDirection', 'tmp'])
+            startTrad = 9.5 * 60 * 60  # 9:30:00.000 in ms after midnight
+            endTrad = 16 * 60 * 60  # 16:00:00.000 in ms after midnight
+
+            theMessageBookFiltered = theMessageBook[theMessageBook['Time'] >= startTrad]
+            theMessageBookFiltered = theMessageBookFiltered[theMessageBookFiltered['Time'] <= endTrad]
+            auctTime = theMessageBookFiltered.loc[theMessageBookFiltered.Type == 6].iloc[0].Time
+            if len(theMessageBookFiltered.loc[theMessageBookFiltered.Type == 6]) > 1:
+                auctTime2 = theMessageBookFiltered.loc[theMessageBookFiltered.Type == 6].iloc[1].Time
+            else:
+                auctTime2 = endTrad
+            theMessageBookFiltered = theMessageBookFiltered[theMessageBookFiltered['Time'] >= auctTime]
+            theMessageBookFiltered = theMessageBookFiltered[theMessageBookFiltered['Time'] <= auctTime2]
+
+            col = ['Ask Price ', 'Ask Size ', 'Bid Price ', 'Bid Size ']
+
+            theNames = []
+            cols = []
+            for i in range(1, 11):
+                for j in col:
+                    if i <= self.nlevels:
+                        cols.append(str(j)+str(i))
+                    theNames.append(str(j) + str(i))
+
+            theOrderBook = pd.read_csv(theOrderBookFileName, names=theNames)
+            theOrderBook = theOrderBook[cols]
+            timeIndex = theMessageBook.index[(theMessageBook.Time >= auctTime) & (theMessageBook.Time <= auctTime2)]
+
+            theOrderBookFiltered = theOrderBook.iloc[timeIndex]
+            # Convert prices into dollars
+            #    Note: LOBSTER stores prices in dollar price times 10000
+
+            for i in list(range(0,len(theOrderBookFiltered.columns),2)):
+                theOrderBookFiltered[theOrderBookFiltered.columns[i]]  = theOrderBookFiltered[theOrderBookFiltered.columns[i]]/10000
+            combinedDf = pd.concat([theMessageBookFiltered , theOrderBookFiltered], axis = 1)
+            combinedDf["Date"] = theMessageBookFileName.split("_")[1]
+            data += [combinedDf]
+        return data
+
+    def loadBinned(self, binLength = 1):
+        data = self.load()
+        orderTypeDict = {'limit' : [1], 'cancel': [2,3], 'market' : [4]}
+        binnedData = []
+        for d in data:
+            binnedL = []
+            for k, v in orderTypeDict.items():
+                l = d.loc[d.Type.apply(lambda x: x in v)]
+                l['count'] = 1
+                bins = np.arange(l.Time.min() - 1e-3, l.Time.max(), binLength)
+                labels = np.arange(0, len(bins)-1, binLength)
+                l['second'] = pd.cut(l['Time'], bins=bins, labels=labels)
+                binL = l.groupby("second").sum()[['count','Size']]
+                binL.reset_index(inplace=True)
+                binnedL.append(binL)
+            binnedData.append(binnedL)
+        return binnedData
+
--- temp.py
+++ temp.py
@@ -3,4 +3,14 @@
 #
 # with py7zr.SevenZipFile('/cs/academic/phd3/konajain/data/AAPL_2019-01-01_2020-09-27_10.7z', mode='r') as z:
 #     z.extractall()
+import datetime as dt
 
+from hawkes import dataLoader
+
+def main():
+    l = dataLoader.Loader("AAPL.OQ", dt.date(2019,1,2), dt.date(2019,1,3), nlevels = 2)
+    data = l.load()
+    print(data[0].head())
+    return
+
+main()
