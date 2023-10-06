7b63f0e919881378b4d9789f560e243763ce0f20
konqr <konark145@gmail.com>
2023-10-06 15:03:00 +0100

    initial commit

---
+++ hawkes/fit.py
@@ -0,0 +1,2 @@
+def fit():
+    print("sgfvsv")
---
+++ temp.py
@@ -0,0 +1,5 @@
+
+import py7zr
+
+with py7zr.SevenZipFile('/cs/academic/phd3/konajain/data/AAPL_2019-01-01_2020-09-27_10.7z', mode='r') as z:
+    z.extractall()
