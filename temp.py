00db3008da389141a715eb314e82b46c2a84688d
konqr <konark145@gmail.com>
2023-10-06 15:45:15 +0100

    init commit

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
+##Extracting data:
+# import py7zr
+#
+# with py7zr.SevenZipFile('/cs/academic/phd3/konajain/data/AAPL_2019-01-01_2020-09-27_10.7z', mode='r') as z:
+#     z.extractall()
 
-import py7zr
-
-with py7zr.SevenZipFile('/cs/academic/phd3/konajain/data/AAPL_2019-01-01_2020-09-27_10.7z', mode='r') as z:
-    z.extractall()
