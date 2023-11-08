580272f7e15a9970c35db9f26f12152f77832534
konqr <konark145@gmail.com>
2023-11-08 14:38:18 +0000

    bug fix

--- temp.py
+++ temp.py
@@ -6,6 +6,7 @@
 import datetime as dt
 import pickle
 from hawkes import dataLoader, fit
+import pandas as pd
 
 def main():
     ric = "AAPL.OQ"
@@ -13,15 +14,15 @@ def main():
     eDate = dt.date(2020,9,24)
     binLength = 0.01
     p = 30
-
-    l = dataLoader.Loader(ric, sDate, eDate, nlevels = 2, dataPath = "/home/konajain/data/")
-    data = l.loadBinned(binLength = binLength, filterTop = True)
-    print(data.keys())
-    T = len(data[sDate.strftime("%Y-%m-%d")]['limit_bid'])
-    cls = fit.ConditionalLeastSquares(data, p, 1, T=T)
-    thetas = cls.fit()
-    with open("/home/konajain/params/" + ric + "_" + str(sDate) + "_" + str(eDate) + "_" + str(binLength) + "_" + str(p) + "_" + str(T) , "wb") as f: #"/home/konajain/params/"
-        pickle.dump(thetas, f)
+    for d in pd.date_range(sDate, eDate):
+        l = dataLoader.Loader(ric, d, d, nlevels = 2, dataPath = "/home/konajain/data/")
+        data = l.loadBinned(binLength = binLength, filterTop = True)
+        print(data.keys())
+        T = len(data[d.strftime("%Y-%m-%d")]['limit_bid'])
+        cls = fit.ConditionalLeastSquares(data, p, 1, T=T)
+        thetas = cls.fit()
+        with open("/home/konajain/params/" + ric + "_" + str(sDate) + "_" + str(eDate) + "_" + str(binLength) + "_" + str(p) + "_" + str(T) , "wb") as f: #"/home/konajain/params/"
+            pickle.dump(thetas, f)
     return thetas
 
 main()
