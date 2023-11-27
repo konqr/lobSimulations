f271fdfe3edab76576e26ba82f137303578176ef
konqr <konark145@gmail.com>
2023-11-27 14:50:09 +0000

    np no pd

--- temp.py
+++ temp.py
@@ -14,17 +14,17 @@ def main():
     sDate = dt.date(2020,9,14)
     eDate = dt.date(2020,9,14)
     for d in pd.date_range(sDate, eDate):
-        l = dataLoader.Loader(ric, d, d, nlevels = 2) #, dataPath = "/home/konajain/data/"
+        l = dataLoader.Loader(ric, d, d, nlevels = 2, dataPath = "/home/konajain/data/")
         #data = l.load12DTimestamps()
-        #df = pd.read_csv("D:\\Work\\PhD\\Data\\AAPL.OQ_2020-09-14_12D.csv")
+        df = pd.read_csv(l.dataPath+"AAPL.OQ_2020-09-14_12D.csv")
         #df = df.loc[df.Time < 100]
-        #eventOrder = np.append(df.event.unique()[6:], df.event.unique()[-7:-13:-1])
-        data = {'2020-09-14 00:00:00' : []} #list(df.groupby('event')['Time'].apply(np.array)[eventOrder].values)}
+        eventOrder = np.append(df.event.unique()[6:], df.event.unique()[-7:-13:-1])
+        data = {'2020-09-14' : list(df.groupby('event')['Time'].apply(np.array)[eventOrder].values)}
         cls = fit.ConditionalLeastSquaresLogLin(data, loader = l)
-        thetas = cls.fit()
-        with open(l.dataPath + ric + "_" + str(sDate) + "_" + str(eDate) + "_CLSLogLin" , "wb") as f: #"/home/konajain/params/"
-            pickle.dump(thetas, f)
-    return thetas
+        cls.runTransformDate()
+        # with open(l.dataPath + ric + "_" + str(sDate) + "_" + str(eDate) + "_CLSLogLin" , "wb") as f: #"/home/konajain/params/"
+        #     pickle.dump(thetas, f)
+    return 0
     # ric = "AAPL.OQ"
     # d = dt.date(2020,9,14)
     # l = dataLoader.Loader(ric, d, d, nlevels = 2, dataPath = "/home/konajain/data/")
