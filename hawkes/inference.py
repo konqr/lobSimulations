03314d6a696f181678950f2ec01fc08f322a58d2
konqr <konark145@gmail.com>
2023-11-28 21:54:02 +0000

    infer params from non parametric fits

--- hawkes/inference.py
+++ hawkes/inference.py
@@ -3,6 +3,8 @@ import pandas as pd
 import statsmodels.api as sm
 import pickle
 import gc
+import datetime as dt
+import dataLoader
 
 class ParametricFit():
 
@@ -37,3 +39,83 @@ class ParametricFit():
             print("Power Law selected")
             thetas = thetasPowerLaw
         return thetas
+
+def run():
+    cols = ["lo_deep_Ask", "co_deep_Ask", "lo_top_Ask","co_top_Ask", "mo_Ask", "lo_inspread_Ask" ,
+            "lo_inspread_Bid" , "mo_Bid", "co_top_Bid", "lo_top_Bid", "co_deep_Bid","lo_deep_Bid" ]
+    ric = "AAPL.OQ"
+    sDate = dt.date(2019,1,2)
+    eDate = dt.date(2019,1,4)
+
+    l = dataLoader.Loader(ric, sDate, eDate, nlevels = 2, dataPath = "/SAN/fca/DRL_HFT_Investigations/LOBSimulations/extracted/")
+    with open(l.dataPath + ric + "_Params_" + str(sDate.strftime("%Y-%m-%d")) + "_" + str(eDate.strftime("%Y-%m-%d")) + "_CLSLogLin_20" , "rb") as f: #"/home/konajain/params/"
+        thetas = pickle.load(f)
+
+    # each theta in kernel = \delta * h(midpoint)
+
+    # 1. plain
+    res = {}
+    params = {}
+    if len(thetas[sDate.strftime('%Y-%m-%d')]) == 2:
+        for d, theta in thetas.iteritems():
+            for i, col in zip(np.arange(12), cols):
+                exo = theta[0][i]
+                res[col] = res.get(col, []) + [exo]
+                phi = theta[1][i,:].reshape((len(theta[1][i,:])//12,12))
+                num_datapoints = (phi.shape[0] + 2)//2
+                min_lag =  1e-3
+                max_lag = 500
+                timegridLin = np.linspace(0,min_lag, num_datapoints)
+                timegridLog = np.exp(np.linspace(np.log(min_lag), np.log(max_lag), num_datapoints))
+                timegrid = np.append(timegridLin[:-1], timegridLog)
+                for j in range(len(cols)):
+                    col2 = cols[j]
+                    points = phi[:,j]
+                    timegrid_len = np.diff(timegrid)/2
+                    timegrid_mid = timegrid[:-1] + timegrid_len
+                    points = points / timegrid_len
+
+                    res[col2 + '->' + col] =  res.get(col2 + '->' + col, []) + [(t,p) for t,p in zip(timegrid_mid, points)]
+        for k, v in res.items():
+            if "->" not in k:
+                params[k] = np.mean(v)
+            else:
+                pars, resTemp = ParametricFit(v).fitPowerLaw()
+                params[k] = pars
+        return params, res
+    # 3. spread + TOD
+    elif len(thetas[sDate.strftime('%Y-%m-%d')]) == 3:
+        newThetas = {}
+        for d, theta in thetas.iteritems():
+            theta1, theta2, theta3 = theta
+            newThetas[d] = np.vstack([theta[:5,:], theta1, theta2, theta[7:,:]])
+        thetas = newThetas
+    # 2. TOD
+    for d, theta in thetas.iteritems():
+        for i, col in zip(np.arange(12), cols):
+            theta_i = theta[i,:]
+            exo = theta_i[:13]
+            res[col] = res.get(col, []) + [exo]
+            phi = theta_i[14:].reshape((len(theta_i[14:])//12,12))
+            num_datapoints = (phi.shape[0] + 2)//2
+            min_lag =  1e-3
+            max_lag = 500
+            timegridLin = np.linspace(0,min_lag, num_datapoints)
+            timegridLog = np.exp(np.linspace(np.log(min_lag), np.log(max_lag), num_datapoints))
+            timegrid = np.append(timegridLin[:-1], timegridLog)
+            for j in range(len(cols)):
+                col2 = cols[j]
+                points = phi[:,j]
+                timegrid_len = np.diff(timegrid)/2
+                timegrid_mid = timegrid[:-1] + timegrid_len
+                points = points / timegrid_len
+
+                res[col2 + '->' + col] =  res.get(col2 + '->' + col, []) + [(t,p) for t,p in zip(timegrid_mid, points)]
+    for k, v in res.items():
+        if "->" not in k:
+            params[k] = np.mean(v)
+        else:
+            pars, resTemp = ParametricFit(v).fitPowerLaw()
+            params[k] = pars
+    return params, res
+
