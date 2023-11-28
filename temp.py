52ac5c0661992dfedbc92ad6096163dee868e7ae
konqr <konark145@gmail.com>
2023-11-28 17:46:39 +0000

    fit cond on day

--- temp.py
+++ temp.py
@@ -77,3 +77,44 @@ def main():
     return 0
 
 main()
+
+# df = pd.read_csv("/SAN/fca/DRL_HFT_Investigations/LOBSimulations/extracted/AAPL.OQ_2019-01-10_12D.csv")
+# eventOrder = np.append(df.event.unique()[6:], df.event.unique()[-7:-13:-1])
+# arrs = list(df.groupby('event')['Time'].apply(np.array)[eventOrder].values)
+# num_datapoints = 10
+# min_lag =  1e-3
+# max_lag = 500
+# timegridLin = np.linspace(0,min_lag, num_datapoints)
+# timegridLog = np.exp(np.linspace(np.log(min_lag), np.log(max_lag), num_datapoints))
+# timegrid = np.append(timegridLin[:-1], timegridLog)
+# timegrid_new = np.floor(timegrid/(timegrid[1] - timegrid[0])).astype(int)
+# ser = []
+# bins = np.arange(0, np.max([np.max(arr) for arr in arrs]) + 1e-9, (timegrid[1] - timegrid[0]))
+# cols = ["lo_deep_Ask", "co_deep_Ask", "lo_top_Ask","co_top_Ask", "mo_Ask", "lo_inspread_Ask" ,
+#         "lo_inspread_Bid" , "mo_Bid", "co_top_Bid", "lo_top_Bid", "co_deep_Bid","lo_deep_Bid" ]
+# for arr, col in zip(arrs, cols):
+#     print(col)
+#     arr = np.max(arr) - arr
+#     assignedBins = np.searchsorted(bins, arr, side="right")
+#     binDf = np.unique(assignedBins, return_counts = True)
+#     binDf = pd.DataFrame({"bin" : binDf[0], col : binDf[1]})
+#     binDf = binDf.set_index("bin")
+#     #binDf = binDf.reset_index()
+#     ser += [binDf]
+#
+# print("done with binning")
+# df = pd.concat(ser, axis = 1)
+# df = df.fillna(0)
+# df = df.sort_index()
+
+
+# res_d = []
+# with open("/SAN/fca/DRL_HFT_Investigations/LOBSimulations/extracted/AAPL.OQ_2019-01-02_2019-01-02_inputRes" , "rb") as f: #"/home/konajain/params/"
+#     while True:
+#         try:
+#             r_d = pickle.load(f)
+#             if len(r_d[0]) == 2:
+#                 r_d = sum(r_d, [])
+#             res_d.append(r_d)
+#         except EOFError:
+#             break
