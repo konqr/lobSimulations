8794300991088953236b534eb78757e6c04eef56
konqr <konark145@gmail.com>
2023-12-28 17:07:30 +0000

    bounds

--- hawkes/fit.py
+++ hawkes/fit.py
@@ -920,11 +920,11 @@ class ConditionalLeastSquaresLogLin():
                 with open(self.cfg.get("loader").dataPath + self.cfg.get("loader").ric + "_Params_" + "2019-01-03" + "_" + "2019-01-03" + "_IS_SCS_bounds" , "rb") as f: #"/home/konajain/params/"
                     thetas_old = pickle.load(f)
                 if id == "oth":
-                    x.value = thetas_old['2019-01-03'][1]
+                    x.value = thetas_old[1]
                 elif id == "inspreadBid":
-                    x.value = thetas_old['2019-01-03'][2]
+                    x.value = thetas_old[2]
                 elif id == "inspreadAsk":
-                    x.value = thetas_old['2019-01-03'][0]
+                    x.value = thetas_old[0]
                 constraints = [constrsX@x <= constrsY, constrsX@x >= -1*constrsY, x >= boundsY_l, x <= boundsY_u]
                 objective = cp.Minimize(0.5 * cp.sum_squares(Xs@x-Ys.reshape(len(Ys), nDim)))
                 prob = cp.Problem(objective, constraints)
