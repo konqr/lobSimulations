64fe02c88b98d4da1561ad5defe01f1821f852dd
konqr <konark145@gmail.com>
2023-12-31 19:20:31 +0000

    boundsDict

--- hawkes/inference.py
+++ hawkes/inference.py
@@ -132,7 +132,7 @@ def run(sDate, eDate, ric = "AAPL.OQ" , suffix  = "_IS_scs", avgSpread = 0.0169,
             else:
                 # numDays = len(v)//len(timegrid_len[1:])
                 norm = np.average(norms[k])
-                if norm < 1e-6:
+                if np.abs(norm) < 1e-6:
                     continue
                 side = np.sign(norm)
                 # if np.abs(norm) > 1: norm = 0.99
