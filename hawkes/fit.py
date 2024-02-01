d647fd1f3f54834799b55216509ababc869ffee1
konqr <konark145@gmail.com>
2024-02-01 15:31:52 +0000

    aslsd

--- hawkes/fit.py
+++ hawkes/fit.py
@@ -13,6 +13,13 @@ from scipy.optimize import lsq_linear
 import osqp
 import scipy as sp
 from scipy import sparse
+import sys
+sys.path.append("/home/konajain/code/aslsd")
+from aslsd.functionals.kernels.basis_kernels. \
+    basis_kernel_exponential import ExponentialKernel
+from aslsd.functionals.kernels.kernel import KernelModel
+from aslsd.models.hawkes.linear.mhp import MHP
+from aslsd.stats.events.process_path import ProcessPath
 
 class ConditionalLeastSquares():
     # Kirchner 2015: An estimation procedure for the Hawkes Process
@@ -839,7 +846,7 @@ class ConditionalLeastSquaresLogLin():
                 pickle.dump(thetas[date], f)
         return thetas
 
-class MLE():
+class ASLSD():
     def __init__(self, data, **kwargs):
         self.data = data
         self.cfg = kwargs
@@ -863,6 +870,20 @@ class MLE():
             except:
                 continue
         df = pd.concat(dfs)
+        dictTimes = df.groupby("event").Time.apply(np.array).to_dict()
+        list_times = [dictTimes[c] for c in cols]
+        list_times = ProcessPath(list_times, 23400)
+        #Define a model
+        d = len(cols)
+        kernel_matrix = [[KernelModel([ExponentialKernel(), ExponentialKernel()]) for j in range(d)]
+                         for i in range(d)]
+        mhp = MHP(kernel_matrix)
+        kwargs = {'is_log_param': True, 'is_log_grad': True}
+        mhp.fit(list_times, 23400, n_iter=1000, seed=1234, verbose=True, **kwargs)
+        fit_log = mhp.fit_log
+        print(fit_log)
+        fig = mhp.plot_solver_path( dpi=None, pad=None, figsize=(15,15), save = True,  filename = "/SAN/fca/Konark_PhD_Experiments/results/aslsd_fit_path_2exp.png")
+        fig = mhp.plot_kernels(dpi=None, figsize=(10,10), save = True,  filename = "/SAN/fca/Konark_PhD_Experiments/results/aslsd_fit_kernels_2exp.png")
 
         return
 
