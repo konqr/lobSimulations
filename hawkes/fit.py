164df5e21777bc890310c2c54077c15d7f818aa0
konqr <konark145@gmail.com>
2024-05-01 13:09:21 +0100

    MLE (common decay)

--- hawkes/fit.py
+++ hawkes/fit.py
@@ -1059,9 +1059,8 @@ class MLE():
         self.data = data
         self.cfg = kwargs
 
-    def fit(self):
-
-        hawkes_learner = HawkesSumExpKern(decays = [1.7e3, 0.9*1.7e3, 0.99*1.7e3, 0.999*1.7e3], verbose = True)
+    def fit(self, tol=1e-6, max_iter=100000, elastic_net_ratio = 0):
+        hawkes_learner = HawkesSumExpKern(decays = [1.7e3, 0.9*1.7e3, 0.99*1.7e3, 0.999*1.7e3], verbose = True, tol=tol, max_iter=max_iter, elastic_net_ratio =elastic_net_ratio)
         hawkes_learner.fit(self.data)
         baseline = hawkes_learner.baseline
         kernels = hawkes_learner.coeffs
