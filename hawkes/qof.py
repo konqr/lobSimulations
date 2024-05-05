4d2d944e19248464b4653e325779f7b1557d2c32
konqr <konark145@gmail.com>
2024-05-05 11:44:14 +0100

    qof bug fix

--- hawkes/qof.py
+++ hawkes/qof.py
@@ -611,6 +611,7 @@ def runInterArrivalTimes(paths, resultsPath, sDate, eDate, ric):
 
 def run(ric = "AAPL.OQ", sDate = dt.date(2019,1,2), eDate = dt.date(2019,3,31), suffix = "_CLSLogLin_10", dataPath = "/SAN/fca/Konark_PhD_Experiments/simulated", resultsPath = "/SAN/fca/Konark_PhD_Experiments/results"):
     paths = [dataPath + "/" + i for i in os.listdir(dataPath) if (ric in i)&(suffix in i)&(~("tmp" in i))]
+    if "Symm2" in ric: ric = ric.replace("Symm2", "")
     if "Symm" in ric: ric = ric.replace("Symm", "")
     # runQQInterArrival(ric, sDate, eDate, resultsPath)
     runSignaturePlots(paths, resultsPath, ric, sDate, eDate)
