40517ba43caae85c89fdb3cbb373ed3f165c832f
konqr <konark145@gmail.com>
2025-03-17 12:03:26 +0000

    HJBQVI - get gpu specs

--- HJBQVI/mm_torch.py
+++ HJBQVI/mm_torch.py
@@ -780,5 +780,5 @@ class MarketMaking():
         return model_phi, model_d, model_u
 
 get_gpu_specs()
-MM = MarketMaking(num_epochs=2000, num_points=100000)
+MM = MarketMaking(num_epochs=2000, num_points=10000)
 MM.train(log_dir = '/SAN/fca/Konark_PhD_Experiments/hjbqvi/logs', model_dir = '/SAN/fca/Konark_PhD_Experiments/hjbqvi/models', layer_widths = [30]*3, n_layers= [5]*3)
