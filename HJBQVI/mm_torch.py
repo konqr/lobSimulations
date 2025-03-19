cf0fb7fa5fe240c2cf6c866530952f129f2d9234
konqr <konark145@gmail.com>
2025-03-19 22:06:10 +0000

    HJBQVI - exp learning decay

--- HJBQVI/mm_torch.py
+++ HJBQVI/mm_torch.py
@@ -779,7 +779,7 @@ class MarketMaking():
         def lr_lambda(epoch):
             # Calculate decay rate
             decay_rate = np.log(1e-4) / (self.EPOCHS - 1)
-            return np.exp(decay_rate * epoch)
+            return np.exp(decay_rate * epoch * 50)
 
         optimizer_phi = optim.Adam(model_phi.parameters(), lr=lr)
         optimizer_u = optim.Adam(model_u.parameters(), lr=lr)
