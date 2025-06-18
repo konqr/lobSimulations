fixed sharpe calculation in npyreader

HawkesRLTrading/npyreader.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-data = np.load("/Users/alirazajafree/researchprojects/logstest_RLagent_only_profit.npy")
+
+data = np.load("/Users/alirazajafree/researchprojects/logs/logstest_and_twap_agents_profit.npy")
 
 times = data[0]
 
@@ -15,8 +16,7 @@ print(boundaryIndices)
 logReturns = []
 
 for i in range(len(boundaryIndices)-1):
-    ret = 0
-    episodicValues = portfolios[boundaryIndices[i]:boundaryIndices[i+1]]
+    episodicValues = [portfolios[boundaryIndices[i]], portfolios[boundaryIndices[i+1]]]
     logReturns.extend(np.diff(np.log(episodicValues)))
 
 logReturns = np.array(logReturns)
@@ -24,5 +24,18 @@ std = np.std(logReturns)
 mean = np.mean(logReturns)
 
 sharpe = mean/std
+print(f"bad sharpe {sharpe}")
 
-print(sharpe)
+
+
+arr = np.load("/Users/alirazajafree/researchprojects/logs/logstest_and_twap_agents_profit.npy")
+episode_boundaries = np.where(np.diff(arr[0]) <0)[0]
+start_idxs = episode_boundaries[:-1] + 1
+end_idxs = episode_boundaries[1:]
+log_ret2 = []
+for s, e in zip(start_idxs, end_idxs):
+    log_ret2.append(np.log(arr[1][e]/arr[1][s]))
+sharpe = np.mean(log_ret2)/np.std(log_ret2)
+ann_sharpe = sharpe*np.sqrt(6.5*12*252)
+print(f"Sharpe: {sharpe}")
+print(f"Ann_sharpe: {ann_sharpe}")