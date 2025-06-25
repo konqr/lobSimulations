3117b8733450ae0b35b083e8b8026f7f1e1eb4c2
arjafree <arjafree@gmail.com>
2025-06-25 21:25:14 +0800

    small fix

--- HawkesRLTrading/AR_RL_runner.py
+++ HawkesRLTrading/AR_RL_runner.py
@@ -4,15 +4,15 @@ sys.path.append(os.path.abspath('/home/ajafree/lobSimulations'))
 from HawkesRLTrading.src.Envs.HawkesRLTradingEnv import *
 import torch
 
-log_dir = 'home/ajafree/researchprojects/logs'
-model_dir = 'home/ajafree/researchprojects/models/icrl_ppo_model_symmetric'
+log_dir = '/home/ajafree/researchprojects/logs'
+model_dir = '/home/ajafree/researchprojects/models/icrl_ppo_model_symmetric'
 
 label = 'test_RLAgent_vs_BUY_TWAP_1000q_1s'
 layer_widths=128
 n_layers=3
 checkpoint_params = ('20250618_115039_inv10_symmHP_lowEpochs_standard', 52)
 
-with open("home/ajafree/researchprojects/otherdata/Symmetric_INTC.OQ_ParamsInferredWCutoffEyeMu_sparseInfer_2019-01-02_2019-12-31_CLSLogLin_10", 'rb') as f: # INTC.OQ_ParamsInferredWCutoff_2019-01-02_2019-03-31_poisson
+with open("/home/ajafree/researchprojects/otherdata/Symmetric_INTC.OQ_ParamsInferredWCutoffEyeMu_sparseInfer_2019-01-02_2019-12-31_CLSLogLin_10", 'rb') as f: # INTC.OQ_ParamsInferredWCutoff_2019-01-02_2019-03-31_poisson
     kernelparams = pickle.load(f)
 kernelparams = preprocessdata(kernelparams)
 
