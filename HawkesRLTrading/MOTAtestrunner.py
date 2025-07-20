1d1d93b578d88c39139d7380ec7a51315ec0ca5b
arjafree <arjafree@gmail.com>
2025-07-20 17:25:18 +0800

    4

--- HawkesRLTrading/MOTAtestrunner.py
+++ HawkesRLTrading/MOTAtestrunner.py
@@ -172,10 +172,10 @@ plt.plot(times, percentage_change_price, alpha=0.5)
 plt.xlabel("Time step")
 plt.ylabel("Midprice")
 plt.title("Price Path Tracking")
-plt.savefig("p_change_price_path_tracking_3.png", dpi=300, bbox_inches='tight')
+plt.savefig("p_change_price_path_tracking_4.png", dpi=300, bbox_inches='tight')
 plt.close()
 
-np.save("p_change_price_path_tracking_3.npy", np.array(percentage_change_price))
+np.save("p_change_price_path_tracking_4.npy", np.array(percentage_change_price))
 
 
 # for agent_id in agent_ids:
